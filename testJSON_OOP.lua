2c51b14affe22fd8db37f2b3afd489dab0b4e78d
jed-richards <jed22richards@gmail.com>
2023-04-04 13:22:05 -0500

    progress on oop now linke updating window and buffer stuff

--- testJSON_OOP.lua
+++ testJSON_OOP.lua
@@ -1,6 +1,43 @@
 local JSON = require("lua.todo.json.json_utils")
 local ITEM = require("oop")
+local LIST = require("lua.todo.list.list")
 
-JSON
+item1 = ITEM:newItem()
+item2 = ITEM:newItem()
 
-ITEM
+--LIST.addItem(item1)
+--LIST.addItem(item2)
+
+--[[
+print("LIST")
+print(vim.inspect(LIST.items))
+
+ITEM:setDesc(item1, 'item1 desc')
+
+print(vim.inspect(item1))
+print(vim.inspect(item2))
+
+ITEM:toggle_state(item1)
+
+print("AFTER TOGGLE")
+
+print(vim.inspect(item1))
+print(vim.inspect(item2))
+
+print("LIST")
+print(vim.inspect(LIST.items))
+
+print()
+
+--LIST.removeAll()
+--]]
+
+--json1 = JSON.encode(item1)
+
+--json_all = JSON.encode(LIST.items)
+
+print(json_all)
+
+items = JSON.decode(json_all)
+
+print(vim.inspect(items))
